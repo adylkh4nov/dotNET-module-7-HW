bbed5113c05b1e2505ae8942534295639c6e3997
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-10-23 19:15:57 +0600

    inital commit

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using WorldOfTanks;
+namespace TankBattleSimulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] t34Tanks = new Tank[5];
+            Tank[] panteraTanks = new Tank[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                t34Tanks[i] = new Tank("T-34");
+                panteraTanks[i] = new Tank("Pantera");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Битва {i + 1}:");
+                Console.WriteLine($"{t34Tanks[i].TankName} vs. {panteraTanks[i].TankName}");
+
+                if (t34Tanks[i] ^ panteraTanks[i])
+                    Console.WriteLine($"{t34Tanks[i].TankName} Победил!");
+                else
+                    Console.WriteLine($"{panteraTanks[i].TankName} Победил!");
+
+                Console.WriteLine(t34Tanks[i].GetTankParameters());
+                Console.WriteLine(panteraTanks[i].GetTankParameters());
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dotNET-module-7-HW/WordOfTanks/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace WorldOfTanks
+{
+    public class Tank
+    {
+        private string Name;
+        private int ammunitionLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+        private static Random random = new Random();
+
+        public Tank(string name)
+        {
+            Name = name;
+            ammunitionLevel = random.Next(0, 101);
+            armorLevel = random.Next(0, 101);
+            maneuverabilityLevel = random.Next(0, 101);
+        }
+
+        public string TankName { get { return Name; } }
+        public int AmmunitionLevel { get { return ammunitionLevel; } }
+        public int ArmorLevel { get { return armorLevel; } }
+        public int ManeuverabilityLevel { get { return maneuverabilityLevel; } }
+
+        public static bool operator ^(Tank t34, Tank pantera)
+        {
+            int t34Wins = 0;
+            int panteraWins = 0;
+
+            if (t34.AmmunitionLevel > pantera.AmmunitionLevel)
+                t34Wins++;
+            else
+                panteraWins++;
+
+            if (t34.ArmorLevel > pantera.ArmorLevel)
+                t34Wins++;
+            else
+                panteraWins++;
+
+            if (t34.ManeuverabilityLevel > pantera.ManeuverabilityLevel)
+                t34Wins++;
+            else
+                panteraWins++;
+
+            return (t34Wins > panteraWins);
+        }
+
+        public string GetTankParameters()
+        {
+            return $"{Name}: Ammo={ammunitionLevel}%, Armor={armorLevel}%, Maneuverability={maneuverabilityLevel}%";
+        }
+    }
+}
